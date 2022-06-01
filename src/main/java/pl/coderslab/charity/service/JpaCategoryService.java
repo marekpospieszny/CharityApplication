8e5ac6f92e3e85fcd34ff777c0f208811423cdc4
Marek <marek.pospieszny@gmail.com>
2022-06-01 22:25:45 +0200

    Service implementation for Category created

---
+++ src/main/java/pl/coderslab/charity/service/JpaCategoryService.java
@@ -0,0 +1,42 @@
+package pl.coderslab.charity.service;
+
+import org.springframework.stereotype.Service;
+import pl.coderslab.charity.entity.Category;
+import pl.coderslab.charity.repository.CategoryRepository;
+
+import java.util.List;
+
+@Service
+public class JpaCategoryService implements CategoryService{
+
+    private final CategoryRepository categoryRepository;
+
+    public JpaCategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    @Override
+    public void add(Category category) {
+        categoryRepository.save(category);
+    }
+
+    @Override
+    public Category get(Long id) {
+        return categoryRepository.getById(id);
+    }
+
+    @Override
+    public void delete(Long id) {
+        categoryRepository.deleteById(id);
+    }
+
+    @Override
+    public void update(Category category) {
+        categoryRepository.save(category);
+    }
+}

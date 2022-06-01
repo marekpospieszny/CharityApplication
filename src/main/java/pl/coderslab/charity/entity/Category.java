7d5f66ea1c4a7bab9a9098fd20c87297d889ccf1
Marek <marek.pospieszny@gmail.com>
2022-06-01 22:24:19 +0200

    Entity Category created

---
+++ src/main/java/pl/coderslab/charity/entity/Category.java
@@ -0,0 +1,25 @@
+package pl.coderslab.charity.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+}

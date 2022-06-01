27075410cd355515370c8e1d9512e21048a8675e
Marek <marek.pospieszny@gmail.com>
2022-06-01 22:59:24 +0200

    Donation entity created

---
+++ src/main/java/pl/coderslab/charity/entity/Donation.java
@@ -0,0 +1,51 @@
+package pl.coderslab.charity.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@Entity
+@Table(name = "donations")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Donation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private Integer quantity;
+
+    @ManyToMany
+    private List<Category> categories;
+
+    @ManyToOne
+    private Institution institution;
+
+    @NotBlank
+    private String street;
+
+    @NotBlank
+    private String city;
+
+    @NotBlank
+    private String zipCode;
+
+    private LocalDate pickUpDate;
+
+    private LocalTime pickUpTime;
+
+    private String pickUpComment;
+
+}

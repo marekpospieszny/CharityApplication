5571cf41546659b534dd7fda52834e0c92df356f
Marek <marek.pospieszny@gmail.com>
2022-06-01 22:40:31 +0200

    Entity Institution created

---
+++ src/main/java/pl/coderslab/charity/entity/Institution.java
@@ -0,0 +1,28 @@
+package pl.coderslab.charity.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "institutions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Institution {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String description;
+}

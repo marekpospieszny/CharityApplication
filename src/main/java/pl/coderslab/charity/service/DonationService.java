715dafa328ea6cd61e3be454820dd242f5b4034c
Marek <marek.pospieszny@gmail.com>
2022-06-01 22:59:59 +0200

    DonationService created

---
+++ src/main/java/pl/coderslab/charity/service/DonationService.java
@@ -0,0 +1,18 @@
+package pl.coderslab.charity.service;
+
+import pl.coderslab.charity.entity.Donation;
+
+import java.util.List;
+
+public interface DonationService {
+
+    List<Donation> getAllDonations();
+
+    void add(Donation donation);
+
+    Donation get(Long id);
+
+    void delete(Long id);
+
+    void update(Donation donation);
+}

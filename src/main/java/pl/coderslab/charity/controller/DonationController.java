311adadec8a8d2d2442b2ef30583c38091a8a125
Marek <marek.pospieszny@gmail.com>
2022-06-02 23:11:24 +0200

    DonationController created

---
+++ src/main/java/pl/coderslab/charity/controller/DonationController.java
@@ -0,0 +1,35 @@
+package pl.coderslab.charity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.entity.Donation;
+import pl.coderslab.charity.service.CategoryService;
+import pl.coderslab.charity.service.DonationService;
+import pl.coderslab.charity.service.InstitutionService;
+
+@Controller
+@RequestMapping("/donation")
+public class DonationController {
+
+    private final CategoryService categoryService;
+    private final InstitutionService institutionService;
+    private final DonationService donationService;
+
+    public DonationController(CategoryService categoryService, InstitutionService institutionService, DonationService donationService) {
+        this.categoryService = categoryService;
+        this.institutionService = institutionService;
+        this.donationService = donationService;
+    }
+
+    @GetMapping("/form")
+    public String addDonationForm(Model model) {
+        model.addAttribute("institutions",institutionService.getAllInstitution());
+        model.addAttribute("categories",categoryService.getAllCategories());
+        model.addAttribute("donation",new Donation());
+        return "form";
+    }
+
+
+}

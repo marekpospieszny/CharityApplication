fcd81d11bd4868421fa9f3dd44f53147610ae9ad
Marek <marek.pospieszny@gmail.com>
2022-06-02 21:35:52 +0200

    HomeController updated - it now provides institutions from database to model

--- src/main/java/pl/coderslab/charity/HomeController.java
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -3,14 +3,21 @@ package pl.coderslab.charity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.service.InstitutionService;
 
 
 @Controller
 public class HomeController {
 
+    InstitutionService institutionService;
+
+    public HomeController(InstitutionService institutionService) {
+        this.institutionService = institutionService;
+    }
 
     @RequestMapping("/")
     public String homeAction(Model model){
+        model.addAttribute("institutions",institutionService.getAllInstitution());
         return "index";
     }
 }

24c84eaa1d40a54e00ec0107945d1fed9165403d
Marek <marek.pospieszny@gmail.com>
2022-06-02 21:58:28 +0200

    DonationRepository updated - added 2 native sql queries: 1. To count all donations. 2. To sum all donations bags.

--- src/main/java/pl/coderslab/charity/repository/DonationRepository.java
+++ src/main/java/pl/coderslab/charity/repository/DonationRepository.java
@@ -1,7 +1,14 @@
 package pl.coderslab.charity.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pl.coderslab.charity.entity.Donation;
 
 public interface DonationRepository extends JpaRepository<Donation, Long> {
+
+    @Query(value = "SELECT COUNT(*) FROM donations", nativeQuery = true)
+    Integer countAllDonations();
+
+    @Query(value = "SELECT SUM(quantity) FROM donations", nativeQuery = true)
+    Integer sumAllDonationsBags();
 }

1a2d17d228187ce5bdae9dd3151ce8ca58ee527c
Marek <marek.pospieszny@gmail.com>
2022-06-03 19:51:42 +0200

    insert.sql updated - to include phone number for Donation

--- src/main/resources/sql/insert.sql
+++ src/main/resources/sql/insert.sql
@@ -11,7 +11,7 @@ VALUES ('ubrania, które nadają się do ponownego użycia'),
        ('książki'),
        ('inne');
 
-INSERT INTO `donations` (city, pick_up_comment, pick_up_date, pick_up_time, quantity, street, zip_code, institution_id)
-VALUES ('Wrocław','comment','2022-06-02','18:30:00',12,'Kiełbaśnicza','55-555',1),
-       ('Poznań','comment2','2022-06-01','09:15:00',1,'Swobodna','55-000',2),
-       ('Legnica','comment3','2022-05-28','20:00:00',2,'Główna','59-123',4);
+INSERT INTO `donations` (city, pick_up_comment, pick_up_date, pick_up_time, quantity, street, zip_code, phone, institution_id)
+VALUES ('Wrocław','comment','2022-06-02','18:30:00',12,'Kiełbaśnicza','55-555',808111222,1),
+       ('Poznań','comment2','2022-06-01','09:15:00',1,'Swobodna','55-000',644222555,2),
+       ('Legnica','comment3','2022-05-28','20:00:00',2,'Główna','59-123',123456789,4);

03807b7021c0c4568c5a5bf7d0cfd4b5966affde
Marek <marek.pospieszny@gmail.com>
2022-06-05 13:20:43 +0200

    DonationController updated - added phone and correct time on donation creation

--- src/main/java/pl/coderslab/charity/controller/DonationController.java
+++ src/main/java/pl/coderslab/charity/controller/DonationController.java
@@ -2,12 +2,22 @@ package pl.coderslab.charity.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.entity.Category;
 import pl.coderslab.charity.entity.Donation;
 import pl.coderslab.charity.service.CategoryService;
 import pl.coderslab.charity.service.DonationService;
 import pl.coderslab.charity.service.InstitutionService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/donation")
@@ -29,6 +39,21 @@ public class DonationController {
         model.addAttribute("categories",categoryService.getAllCategories());
         model.addAttribute("donation",new Donation());
         return "form";
+    }
+
+    @PostMapping("/form")
+    public String acceptDonation(@Valid Donation donation, BindingResult result, Model model, HttpServletRequest  request) {
+        if(result.hasErrors()) {
+            model.addAttribute("institutions",institutionService.getAllInstitution());
+            model.addAttribute("categories",categoryService.getAllCategories());
+            return "form";
+        }
+        Long institutionId = Long.parseLong(request.getParameter("organization"));
+        donation.setInstitution(institutionService.get(institutionId));
+        LocalTime time = LocalTime.parse(request.getParameter("time"));
+        donation.setPickUpTime(time);
+        donationService.add(donation);
+        return "redirect:/";
     }
 
 

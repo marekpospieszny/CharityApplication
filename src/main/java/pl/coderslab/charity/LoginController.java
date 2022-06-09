6fefb50477dd60d698c3b849cbb75a0c9bf016ef
Marek <marek.pospieszny@gmail.com>
2022-06-09 19:40:09 +0200

    LoginController added

---
+++ src/main/java/pl/coderslab/charity/LoginController.java
@@ -0,0 +1,22 @@
+package pl.coderslab.charity;
+
+import lombok.Getter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping(value = {"/login"}, method = RequestMethod.GET)
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register(Model model) {
+        return "register";
+    }
+}

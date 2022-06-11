a200bc8b14c42ae8298fd1d683e892b93378bfbb
Marek <marek.pospieszny@gmail.com>
2022-06-11 13:30:16 +0200

    CurrentUser created

---
+++ src/main/java/pl/coderslab/charity/entity/CurrentUser.java
@@ -0,0 +1,19 @@
+package pl.coderslab.charity.entity;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+public class CurrentUser extends org.springframework.security.core.userdetails.User {
+
+    private final User user;
+
+    public CurrentUser(String email, String password, Collection<? extends GrantedAuthority> authorities, User user) {
+        super(email,password,authorities);
+        this.user = user;
+    }
+    public User getUser() {
+        return user;
+    }
+
+}

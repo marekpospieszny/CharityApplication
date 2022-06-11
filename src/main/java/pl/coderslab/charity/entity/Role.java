787820903518963492a3794ab629180a6012bc25
Marek <marek.pospieszny@gmail.com>
2022-06-11 13:24:19 +0200

    Role entity created

---
+++ src/main/java/pl/coderslab/charity/entity/Role.java
@@ -0,0 +1,22 @@
+package pl.coderslab.charity.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+}

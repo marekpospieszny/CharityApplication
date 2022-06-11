009e667774558fdea5aebacde46f5fe22b73450c
Marek <marek.pospieszny@gmail.com>
2022-06-11 13:26:41 +0200

    SpringBoot configured: created SecurityConfig class

---
+++ src/main/java/pl/coderslab/charity/SecurityConfig.java
@@ -0,0 +1,45 @@
+package pl.coderslab.charity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import pl.coderslab.charity.service.SpringDataUserDetailsService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user1").password("{noop}user123").roles("USER")
+                .and()
+                .withUser("admin1").password("{noop}admin123").roles("ADMIN");
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/home").permitAll()
+                .antMatchers("/register").permitAll()
+                .antMatchers("/about").authenticated()
+                .and().formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/");
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SpringDataUserDetailsService customUserDetailsService() {
+        return new SpringDataUserDetailsService();
+    }
+
+}
